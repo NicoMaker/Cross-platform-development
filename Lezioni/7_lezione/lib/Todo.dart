1e7e4df09b621177e774b9c23e4d680f70e908d6
NicoMaker <nicola.marano02@gmail.com>
2025-11-04 10:14:11 +0100

    add 7 ex

---
+++ Lezioni/7_lezione/lib/Todo.dart
@@ -0,0 +1,26 @@
+
+class Todo {
+  Todo({
+    required this.createdAt,
+    required this.title,
+    required this.description,
+    this.isDone = false,
+    this.expiresAt,
+  });
+  bool isDone;
+  String title;
+  String description;
+  DateTime createdAt;
+  DateTime? expiresAt;
+}
+
+void f() {
+  final todo = Todo(
+    createdAt: DateTime.now(),
+    title: "UN POLLOOOOO",
+    description: "fagiano",
+    isDone: true,
+    expiresAt: DateTime(2026),
+  );
+  print(todo.isDone); // false
+}
---
+++ Lezioni/7_lezione/lib/form.dart
@@ -0,0 +1,86 @@
+import "package:flutter/material.dart";
+import "package:its_aa_pn_2025_cross_platform/todo.dart";
+import "package:reactive_forms/reactive_forms.dart";
+
+class AddTodoFormDialog extends StatefulWidget {
+  const AddTodoFormDialog({super.key});
+
+  @override
+  State<AddTodoFormDialog> createState() => _AddTodoFormDialogState();
+}
+
+class _AddTodoFormDialogState extends State<AddTodoFormDialog> {
+  late final FormGroup _form;
+
+  @override
+  void initState() {
+    super.initState();
+    _form = FormGroup({
+      "title": FormControl<String>(
+        value: "",
+        validators: [const RequiredValidator(), const MinLengthValidator(3)],
+      ),
+      "description": FormControl<String>(
+        value: "",
+        validators: [const RequiredValidator(), const MinLengthValidator(20)],
+      ),
+      "t&c": FormControl<bool>(value: false, validators: [const RequiredValidator()]),
+    });
+  }
+
+  @override
+  void dispose() {
+    _form.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Dialog(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: ReactiveForm(
+          formGroup: _form,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("nuovo todo!", style: theme.textTheme.headlineSmall),
+              const SizedBox(height: 40),
+              ReactiveTextField(
+                formControlName: "title",
+                decoration: const InputDecoration(hintText: "titolo..."),
+              ),
+              ReactiveTextField(
+                formControlName: "description",
+                decoration: const InputDecoration(hintText: "descrizione..."),
+              ),
+              const SizedBox(height: 20),
+              ReactiveCheckboxListTile(
+                formControlName: "t&c",
+                contentPadding: EdgeInsets.zero,
+                title: const Text("accetto i t&c"),
+              ),
+              const SizedBox(height: 80),
+              ElevatedButton(onPressed: _submit, child: const Text("salva!")),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _submit() {
+    if (!_form.valid) return;
+
+    final todo = Todo(
+      createdAt: DateTime.now(),
+      title: _form.control("title").value as String,
+      description: _form.control("description").value as String,
+      isDone: _form.control("t&c").value as bool,
+    );
+
+    Navigator.pop(context, todo);
+  }
+}
